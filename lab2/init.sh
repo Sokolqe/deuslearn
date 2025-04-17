c6862bfbfde01bc536a121cc36fd36c36670a6b6
Andrey Sokolov <sokolqe@gmail.com>
2025-04-17 15:19:41 +0000

    Added lab2

---
+++ lab2/init.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# User credentials
+#user=admin
+#email=admin@example.com
+#password=pass
+#file=db/db.sqlite3
+
+if [[ ! -f $file ]]; then
+	python3 manage.py migrate
+        echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell || true
+else
+	echo "Database exists, running server..."
+fi
+
+
+python3 manage.py runserver 0.0.0.0:8000

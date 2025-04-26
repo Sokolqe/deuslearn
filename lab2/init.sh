ee70ed85ac8303986e02c5d2397587a939fd2acf
Andrey Sokolov <sokolqe@gmail.com>
2025-04-26 15:42:28 +0000

    Edited lab2 Dockerfile, compose file, env file

--- lab2/init.sh
+++ lab2/init.sh
@@ -1,16 +1,10 @@
 #! /bin/sh
 
-# User credentials
-#user=admin
-#email=admin@example.com
-#password=pass
-#file=db/db.sqlite3
+python3 manage.py migrate
 
-if [ ! -f $file ]; then
-	python3 manage.py migrate
-        echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+if [ $(echo "from django.contrib.auth.models import User; print(User.objects.filter(username='$user').exists())" | python3 manage.py shell) == True ]; then 
+	echo "Superuser $user already exists. Running server..."
 else
-	python3 manage.py migrate
-	echo "Database exists, running server..."
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+	echo "Superuser $user created. Running server..."
 fi
-
